0a86d3aa9aa6b884ad72723e861f25e73ddf1794
David Enke <david@enke.dev>
2022-11-27 01:53:15 +0100

    build: exclude deps from artifact

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,5 +1,6 @@
 import { build } from 'esbuild';
 import { clean } from 'esbuild-plugin-clean';
+import { dependencies } from './package.json';
 
 const isWatchMode = process.argv.includes('--watch');
 
@@ -12,6 +13,7 @@ build({
   minify: !isWatchMode,
   sourcemap: isWatchMode,
   watch: isWatchMode,
+  external: Object.keys(dependencies),
   plugins: [clean({ patterns: ['./dist'] })]
 })
   .then(() => !isWatchMode && process.exit(0))
